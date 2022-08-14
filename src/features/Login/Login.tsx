5459e4377f828f6d17d5f60f45104ad7f89960b3
hai.dinh <hai.dinh@amela.vn>
2022-08-15 01:40:53 +0700

    fist commit

---
+++ src/features/Login/Login.tsx
@@ -0,0 +1,105 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
+// material core
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import { makeStyles } from '@mui/material/styles';
+import Container from '@mui/material/Container';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
+// material icon
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+
+// configs
+import { USER_ROLE } from 'configs';
+
+// actions
+import { login } from 'actions/auth.action';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function SignIn() {
+  const classes = useStyles();
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const [name, setName] = useState('tonynguyen');
+  const [role, setRole] = useState('ADMIN');
+
+  const _onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    dispatch(login(name, role, history));
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Login
+        </Typography>
+        <form className={classes.form} noValidate onSubmit={_onSubmit}>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="name"
+            label="User Name"
+            name="name"
+            autoComplete="name"
+            autoFocus
+            value={name}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setName(e.target.value)}
+          />
+          <br />
+          <br />
+          <FormControl variant="outlined" fullWidth>
+            <InputLabel id="demo-simple-select-outlined-label">Role</InputLabel>
+            <Select
+              fullWidth
+              value={role}
+              onChange={(e: React.ChangeEvent<{ value: unknown }>) => setRole(e.target.value as string)}
+              label="Role"
+            >
+              <MenuItem value={USER_ROLE.ADMIN}>Admin</MenuItem>
+              <MenuItem value={USER_ROLE.LEAD}>Lead</MenuItem>
+              <MenuItem value={USER_ROLE.GUEST}>Guest</MenuItem>
+            </Select>
+          </FormControl>
+          <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
+            Submit
+          </Button>
+        </form>
+      </div>
+    </Container>
+  );
+}
---
+++ src/features/Users/Users.tsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { makeStyles } from '@mui/material/styles';
+import Grid from '@mui/material/Grid';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import IconButton from '@mui/material/IconButton';
+import EditIcon from '@material-ui/icons/Edit';
+import AddIcon from '@material-ui/icons/Add';
+import Button from '@mui/material/Button';
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+});
+
+function createData(name: string, email: string, role: string) {
+  return { name, email, role };
+}
+
+const rows = [
+  createData('Tony Nguyen', 'nhattruong1811@gmail.com', 'ADMIN'),
+  createData('David Name', 'david@gmail.com', 'OPERATOR'),
+];
+
+export default function UserList() {
+  const classes = useStyles();
+  const history = useHistory();
+
+  return (
+    <>
+      <Grid container alignItems="center">
+        <Grid sm={8}>
+          <h2>User</h2>
+        </Grid>
+        <Grid sm={4} container justify="flex-end">
+          <Button
+            variant="contained"
+            color="primary"
+            size="small"
+            startIcon={<AddIcon />}
+            onClick={() => history.push('/user/add')}
+          >
+            Add
+          </Button>
+        </Grid>
+      </Grid>
+      <TableContainer component={Paper}>
+        <Table className={classes.table} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell>Email</TableCell>
+              <TableCell>Role</TableCell>
+              <TableCell>Action</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow key={row.name}>
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell>{row.email}</TableCell>
+                <TableCell>{row.role}</TableCell>
+                <TableCell>
+                  <IconButton color="primary" aria-label="edit user" component="span">
+                    <EditIcon />
+                  </IconButton>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
+  );
+}
